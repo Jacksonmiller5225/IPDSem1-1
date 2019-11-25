8c12dcda34ac3f70054dc322a8d45d214c03869f
Jackson Miller <225022@shakopeeschools.org>
2019-11-25 14:09:35 -0600

    changed the name, strategy, and description
    changed the name, strategy, and description

--- team18.py
+++ team18.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Jackson' # Only 10 chars displayed.
+strategy_name = 'copy their move 2 rounds ago'
+strategy_description = 'It basicaly copies the move they made 2 move ago'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
